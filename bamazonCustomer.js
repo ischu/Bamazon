d5f832b531fd4cf91a591af8dc635e4f7eb392f3
ianschu <ianschu7@gmail.com>
2019-02-09 14:46:17 -0500

    item check function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,8 +10,16 @@
 // 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
 //    * This means updating the SQL database to reflect the remaining quantity.
 //    * Once the update goes through, show the customer the total cost of their purchase.
+var mysql = require("mysql");
+var inquire = require("inquirer");
 
-inquire = require("inquirer");
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "tara1",
+    database: "bamazon"
+});
 
 inquire.prompt([
     {
@@ -24,23 +32,56 @@ inquire.prompt([
         type: "input",
         message: "How many items do you wish to purchase?"
     }
-]).then(function(answ){
-// if id exists and quantity > requested amount
-itemCheck(answ.id, answ.amount);
-// adjust db and console.log cost
-// else{
+]).then(function (answ) {
+    connection.connect(function (err) {
+        if (err) throw err;
+        console.log("connected to Bamazon \n");
+        itemCheck(answ.id, answ.amount);
+        connection.end();
+    });
+    // if id exists and quantity > requested amount
+    // adjust db and console.log cost
+    // else{
     // console.log("Order cannot be completed- insufficient quantity!");
-// }
+    // }
 });
 
-// looks up item's id and checks if quantity > requested amount
-itemCheck=function(id, amount){
+// function readProducts() {
+//     console.log("Selecting all products...\n");
+//     connection.query("SELECT * FROM products", function(err, res) {
+//       if (err) throw err;
+//       // Log all results of the SELECT statement
+//       console.log(res);
+//       connection.end();
+//     });
+//   }
 
+// looks up item's id and checks if quantity > requested amount
+itemCheck = function (id, amount) {
+    connection.query(
+        "SELECT stock_quantity FROM products WHERE ?",
+        {
+            item_id: id,
+        },
+        function (err, res) {
+            console.log(res[0].stock_quantity);
+            if (err) {
+                throw err
+            };
+            // if amount stocked is greater or equal to amount requested
+            if (res[0].stock_quantity > amount) {
+                // run function for order successfully fulfilled
+                orderFulfilled(id, amount);
+            } else {
+                console.log("Order cannot be completed- insufficient quantity!");
+            }
+        }
+    )
 };
 
 // adjusts db, calc and log cost
-orderFulfilled=function(id, amount){
-
+orderFulfilled = function (id, amount) {
+    console.log("You purchased "+amount+" items with id "+id+"!");
 };
 
 
